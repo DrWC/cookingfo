a456b9597abdacd54344ad524a8346704460756b
DrWC <dfg332@ruu.kr>
2018-11-26 22:08:07 +0900

    대기

--- aaa.js
+++ aaa.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
            $("#keyword").keyup(function() {
                var k = $(this).val();
                $("#user-table > tbody > tr").hide();
-               var temp = $("#user-table > tbody > tr > td:nth-child(5n+2):contains('" + k + "')");
+               var temp = $("#user-table > tbody > tr > td:nth-child(1n+1):contains('" + k + "')");
 
                $(temp).parent().show();
            })
---
+++ db_info.php
@@ -0,0 +1,17 @@
+
+<?php
+$conn = mysql_connect("localhost/cookingfo", "root", "SFDASF32523qrfASEF");
+        mysql_select_db("cookworld",$conn);
+?>
+
+
+
+//mysqli_qurery($conn, "
+//INSERT INTO topic
+  //   (title, description, created)
+    // VALUE(
+      // 'MySQL',
+    //   'MySQL is ..',
+    //   NOW()
+    //   )
+    //   ");
